8c6a5f2c576bd934ef8bd680e9ae8b65b45d1987
Wesley Cho <wesley.cho@gmail.com>
2013-06-01 11:22:34 -0400

    Fixed testing using grunt

--- gruntFile.js
+++ gruntFile.js
@@ -5,6 +5,11 @@ module.exports = function (grunt) {
   // Default task.
   grunt.registerTask('default', ['karma']);
 
+  var karmaConfig = function(configFile, customOptions) {
+    var options = { configFile: configFile, keepalive: true };
+    return grunt.util._.extend(options, customOptions);
+  };
+
   // Project configuration.
   grunt.initConfig({
     karma: {
