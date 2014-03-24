91b910ecef488afa53b83157eb20e217aac6ff01
codef0rmer <amit.2006.it@gmail.com>
2014-03-24 16:32:17 +0530

    chore(*): fix jQuery references in demos and tests

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,7 +2,7 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/jquery/jquery.js',
+  'components/jquery/dist/jquery.js',
   'components/jquery-ui/ui/minified/jquery-ui.min.js',
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
