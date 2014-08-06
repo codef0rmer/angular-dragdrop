c2108ffaabd9d5f155e26bc86a96335ecb8453cb
Tommy <snowman4415@gmail.com>
2014-08-06 15:52:18 -0500

    fixed jquery-ui dependency location & test

--- test/test.conf.js
+++ test/test.conf.js
@@ -3,7 +3,7 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'components/jquery/dist/jquery.js',
-  'components/jquery-ui/ui/minified/jquery-ui.min.js',
+  'components/jquery-ui/jquery-ui.min.js',
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
   'src/angular-dragdrop.js',
