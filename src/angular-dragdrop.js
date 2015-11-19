7d664f0a5574afb76bf11a09b75e4ff6f01534f2
Igor Iakymenko <yakimko@gmail.com>
2015-11-19 12:33:16 +0200

    fixed destroy of the plugin

--- src/angular-dragdrop.js
+++ src/angular-dragdrop.js
@@ -337,7 +337,7 @@ var jqyoui = angular.module('ngDragDrop', []).service('ngDragDropService', ['$ti
         killWatcher = scope.$watch(function() { return scope.$eval(attrs.drag); }, updateDraggable);
         updateDraggable();
 
-        element.on('$destroy', function() {
+        scope.$on('$destroy', function() {
           element.draggable({disabled: true}).draggable('destroy');
         });
       }
@@ -400,7 +400,7 @@ var jqyoui = angular.module('ngDragDrop', []).service('ngDragDropService', ['$ti
         killWatcher = scope.$watch(function() { return scope.$eval(attrs.drop); }, updateDroppable);
         updateDroppable();
         
-        element.on('$destroy', function() {
+        scope.$on('$destroy', function() {
           element.droppable({disabled: true}).droppable('destroy');
         });
       }
