be15547109dbdbfe877556e924d60c5f5e845b05
Andre Albino Pereira <andre@softplan.com.br>
2017-10-29 11:33:06 -0200

    Removes window.setTimeout dependency
    Fixed to use `$timeout` instead of `window.setTimeout`. That allowed
    removing `window` dependency from `ngDragDropService`.

--- src/angular-dragdrop.js
+++ src/angular-dragdrop.js
@@ -27,7 +27,7 @@
  * (c) 2013 Amit Gharat a.k.a codef0rmer <amit.2006.it@gmail.com> - amitgharat.wordpress.com
  */
 
-(function (window, angular, $, undefined) {
+(function (angular, $, undefined) {
 'use strict';
 
 var jqyoui = angular.module('ngDragDrop', []).service('ngDragDropService', ['$timeout', '$parse', '$q', function($timeout, $parse, $q) {
@@ -173,7 +173,7 @@ var jqyoui = angular.module('ngDragDrop', []).service('ngDragDropService', ['$ti
     this.move = function($fromEl, $toEl, toPos, duration, dropSettings, callback) {
       if ($fromEl.length === 0) {
         if (callback) {
-          window.setTimeout(function() {
+          $timeout(function() {
             callback();
           }, 300);
         }
@@ -416,4 +416,4 @@ var jqyoui = angular.module('ngDragDrop', []).service('ngDragDropService', ['$ti
 
     return element.getAttribute(name) || element.getAttribute('data-' + name);
   };
-})(window, window.angular, window.jQuery);
+})(window.angular, window.jQuery);
